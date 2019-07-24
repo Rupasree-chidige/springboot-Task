8fdc4ea10118551f366662e5016aa79e39fb7037
Rupasree <rupasree045@gmail.com>
2019-07-24 16:52:19 +0530

    fourth task

--- src/main/java/com/stackroute/userservice/config/WebConfiguration.java
+++ src/main/java/com/stackroute/userservice/config/WebConfiguration.java
@@ -5,6 +5,14 @@ import org.apache.catalina.servlets.WebdavServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class WebConfiguration {
@@ -13,5 +21,22 @@ public class WebConfiguration {
         ServletRegistrationBean registrationBean=new ServletRegistrationBean(new WebdavServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
+    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.stackroute.Muzix.controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo()).tags(new Tag("Track Controller", "Operations pertaining to Tracks in Track Management"));
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("Track Management REST API")
+
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
     }
 }

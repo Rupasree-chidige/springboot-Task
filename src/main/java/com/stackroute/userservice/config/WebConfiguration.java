416c2ffcea692b217699e258de2da3c7abb8ba56
Rupasree <rupasree045@gmail.com>
2019-07-24 15:35:00 +0530

    h2 Console

---
+++ src/main/java/com/stackroute/userservice/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.stackroute.userservice.config;
+
+
+import org.apache.catalina.servlets.WebdavServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean=new ServletRegistrationBean(new WebdavServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}

d5cdff9573bb5dee483b86461009befde3ecc8cd
Rupasree <rupasree045@gmail.com>
2019-07-25 10:47:40 +0530

    seventh-task

---
+++ src/main/java/com/stackroute/userservice/exceptions/TrackControllerAdvivce.java
@@ -0,0 +1,26 @@
+package com.stackroute.userservice.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice("com.stackroute.userservice.controller")
+class TrackControllerAdvice extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(value = {TrackNotFoundException.class})
+  protected ResponseEntity<Object> handleNotFoundConflict(Exception ex, WebRequest request) {
+    String bodyOfResponse = "Track not found!";
+    return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+  }
+
+  @ExceptionHandler(value = {TrackAlreadyExistsException.class})
+  protected ResponseEntity<Object> handleAlreadyExistsConflict(Exception ex, WebRequest request) {
+    String bodyOfResponse = "Track already exists!";
+    return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+  }
+
+}
